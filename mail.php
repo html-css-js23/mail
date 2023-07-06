a2e6e52706f1f65ceee7eab3909991316250b969
html-css-js23 <129732023+html-css-js23@users.noreply.github.com>
2023-07-06 10:21:34 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
-$to = 'recipient@example.com';
+$to = 'alexanderv@isbreda.com';
 $subject = 'Hello, world!';
 $message = 'This is a test email from PHP.';
-$headers = 'From: sender@example.com';
+$headers = 'From: alexanderv@isbreda.com';
 
 if (mail($to, $subject, $message, $headers)) {
     echo 'Email sent successfully!';
